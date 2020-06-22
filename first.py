29163ffa73bff0a0fd63dea42d3a7c1fa771cc5e
Manthan Keim <manthankeim7@gmail.com>
2020-06-22 23:53:12 +0530

    Update first.py

--- first.py
+++ first.py
@@ -7,7 +7,7 @@ import cv2
 img = cv2.imread("hello.jpg")
 cv2.line(img , (300,300) , (500,750), (0,0,0), 2)
 # cv2.rectangle(img , (100,100) , (200,200), (255,100,255), 2)
-cv2.imshow("my_img" , img)
+cv2.imshow("my_img1" , img)
 
 
 
